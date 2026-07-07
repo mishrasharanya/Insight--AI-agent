aaadf73b09e678691fee1c07051111ce405ca691
Sharanya Mishra <sharanyamishra8@gmail.com>
2026-07-07 16:09:54 -0500

    api.py modified for vercel

--- api.py
+++ api.py
@@ -23,6 +23,7 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[FRONTEND_URL, "http://localhost:3000", "http://localhost:5173"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
